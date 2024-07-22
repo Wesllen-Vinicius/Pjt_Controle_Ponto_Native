98c8445ca36a17f6ebbef9c1a14954e2c4aeed40
Wesllen Lima <wesllenvinicius09@gmail.com>
2024-07-22 19:54:35 -0400

    [Historico][estilizacao tela historico]

--- app/(tabs)/historico.tsx
+++ app/(tabs)/historico.tsx
@@ -1,73 +1,251 @@
-import "react-native-gesture-handler";
+import React, { useState } from "react";
+import {
+  SafeAreaView,
+  StatusBar,
+  TouchableOpacity,
+  FlatList,
+  View,
+  Text,
+  StyleSheet,
+  ViewStyle,
+  TextStyle,
+} from "react-native";
+import Header from "@/components/Header";
+import { useDarkMode } from "@/context/DarkModeContext";
+import RNDateTimePicker from "@react-native-community/datetimepicker";
 
-import { Image, StyleSheet, Platform } from "react-native";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+const exampleRecords = [
+  {
+    date: "15/07/2024",
+    records: [
+      { time: "08:00", type: "Entrada" },
+      { time: "12:00", type: "Saída para almoço" },
+      { time: "13:00", type: "Retorno do almoço" },
+      { time: "17:00", type: "Saída" },
+    ],
+  },
+  {
+    date: "14/07/2024",
+    records: [
+      { time: "08:05", type: "Entrada" },
+      { time: "17:05", type: "Saída" },
+    ],
+  },
+];
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
+const HistoryScreen = () => {
+  const [expandedDay, setExpandedDay] = useState<string | null>(null);
+  const [startDate, setStartDate] = useState<Date | undefined>(undefined);
+  const [endDate, setEndDate] = useState<Date | undefined>(undefined);
+  const [showStartDatePicker, setShowStartDatePicker] = useState(false);
+  const [showEndDatePicker, setShowEndDatePicker] = useState(false);
+  const { isDarkMode } = useDarkMode();
+
+  const handleToggleExpand = (date: string) => {
+    setExpandedDay(expandedDay === date ? null : date);
+  };
+
+  const renderRecordItem = ({
+    item,
+  }: {
+    item: { time: string; type: string };
+  }) => (
+    <View
+      style={[
+        styles.recordItem,
+        { backgroundColor: isDarkMode ? "#161B22" : "#FFFFFF" },
+      ]}
     >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Home</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit{" "}
-          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
-          to see changes. Press{" "}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: "cmd + d", android: "cmd + m" })}
-          </ThemedText>{" "}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this
-          starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{" "}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
-          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
-          directory. This will move the current{" "}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <Text
+        style={[
+          styles.recordText,
+          { color: isDarkMode ? "#FFFFFF" : "#070707" },
+        ]}
+      >
+        {item.time} - {item.type}
+      </Text>
+    </View>
   );
-}
+
+  const renderDayItem = ({ item }: { item: (typeof exampleRecords)[0] }) => (
+    <View
+      style={[
+        styles.dayContainer,
+        {
+          backgroundColor: isDarkMode ? "#161B22" : "#FFFFFF",
+          borderColor: isDarkMode ? "#1F1B1B" : "#DDDDDD",
+        },
+        expandedDay === item.date ? styles.selectedDay : {},
+      ]}
+    >
+      <TouchableOpacity
+        style={styles.dayHeader}
+        onPress={() => handleToggleExpand(item.date)}
+      >
+        <Text
+          style={{ color: isDarkMode ? "#FFFFFF" : "#070707", fontSize: 16 }}
+        >
+          {item.date}
+        </Text>
+      </TouchableOpacity>
+      {expandedDay === item.date && (
+        <FlatList
+          data={item.records}
+          renderItem={renderRecordItem}
+          keyExtractor={(record) => record.time}
+          style={styles.recordList}
+        />
+      )}
+    </View>
+  );
+
+  return (
+    <SafeAreaView
+      style={[
+        styles.container,
+        { backgroundColor: isDarkMode ? "#010409" : "#F6F8FA" },
+      ]}
+    >
+      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+      <Header appName="Histórico" />
+      <View style={styles.content}>
+        <View style={styles.dateRangeContainer}>
+          <TouchableOpacity
+            onPress={() => setShowStartDatePicker(true)}
+            style={[
+              styles.dateInput,
+              { backgroundColor: isDarkMode ? "#161B22" : "#FFFFFF" },
+            ]}
+          >
+            <Text style={{ color: isDarkMode ? "#FFFFFF" : "#070707" }}>
+              {startDate ? startDate.toLocaleDateString() : "Início"}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => setShowEndDatePicker(true)}
+            style={[
+              styles.dateInput,
+              { backgroundColor: isDarkMode ? "#161B22" : "#FFFFFF" },
+            ]}
+          >
+            <Text style={{ color: isDarkMode ? "#FFFFFF" : "#070707" }}>
+              {endDate ? endDate.toLocaleDateString() : "Fim"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+        {showStartDatePicker && (
+          <RNDateTimePicker
+            value={startDate || new Date()}
+            mode="date"
+            display="default"
+            onChange={(event, selectedDate) => {
+              setShowStartDatePicker(false);
+              if (selectedDate) {
+                setStartDate(selectedDate);
+              }
+            }}
+          />
+        )}
+        {showEndDatePicker && (
+          <RNDateTimePicker
+            value={endDate || new Date()}
+            mode="date"
+            display="default"
+            onChange={(event, selectedDate) => {
+              setShowEndDatePicker(false);
+              if (selectedDate) {
+                setEndDate(selectedDate);
+              }
+            }}
+          />
+        )}
+        <TouchableOpacity
+          style={[
+            styles.searchButton,
+            { backgroundColor: isDarkMode ? "#1F1B1B" : "#1C8139" },
+          ]}
+        >
+          <Text
+            style={{ color: isDarkMode ? "#FFFFFF" : "#FFFFFF", fontSize: 16 }}
+          >
+            Buscar
+          </Text>
+        </TouchableOpacity>
+        <FlatList
+          data={exampleRecords}
+          renderItem={renderDayItem}
+          keyExtractor={(item) => item.date}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  container: { flex: 1 },
+  content: {
+    flex: 1,
+    marginTop: 16,
+    paddingHorizontal: 16,
+  } as ViewStyle,
+  dateRangeContainer: {
     flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 16,
+  } as ViewStyle,
+  dateInput: {
+    borderWidth: 0.2,
+    flex: 1,
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    borderRadius: 4,
     alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
+    marginHorizontal: 8,
+  } as ViewStyle,
+  searchButton: {
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+    alignItems: "center",
+    marginBottom: 16,
+    width: "100%",
+  } as ViewStyle,
+  dayContainer: {
     marginBottom: 8,
+    borderRadius: 8,
+    overflow: "hidden",
+    width: "100%",
+    borderWidth: 1,
+  } as ViewStyle,
+  dayHeader: {
+    padding: 16,
+    borderRadius: 8,
+    width: "100%",
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#000000",
+  } as ViewStyle,
+  recordList: {
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderTopWidth: 1,
+    borderTopColor: "#DDDDDD",
+  } as ViewStyle,
+  recordItem: {
+    paddingVertical: 12,
+    borderBottomWidth: 1,
+    borderBottomColor: "#DDDDDD",
+    width: "100%",
+  } as ViewStyle,
+  selectedDay: {
+    color: "#ffffff",
+    borderColor: "#A8C1FF",
+    borderWidth: 2,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
+  recordText: {
+    fontSize: 16,
+    lineHeight: 24,
+  } as TextStyle,
 });
+
+export default HistoryScreen;
